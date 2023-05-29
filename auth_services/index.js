3caefd67679d2d39cac504f7d05e44ca483890e2
lahlaili <mohamedlahlaili246@gmail.com>
2023-05-29 23:58:39 +0100

    connect to database on auth_services

--- auth_services/index.js
+++ auth_services/index.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 5000
 const mongoose = require('mongoose')
 
-mongoose.connect()
+mongoose.connect("mongodb://127.0.0.1/auth_services")
+mongoose.connection.on("connected",()=>{
+    console.log('auth_services mongodb connected')
+})
 
-app.use(express.json)
+
+app.use(express.json())
 
 app.listen(port,()=>{
     console.log(`auth services is running at ${port}`)
