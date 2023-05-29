3c6a66b4387a5648cd59700461e41a69e15c809f
lahlaili <mohamedlahlaili246@gmail.com>
2023-05-30 00:16:18 +0100

    create user testing

--- auth_services/index.js
+++ auth_services/index.js
@@ -2,11 +2,33 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const User = require('./user.model')
+
 
 mongoose.connect("mongodb://127.0.0.1/auth_services")
 mongoose.connection.on("connected",()=>{
     console.log('auth_services mongodb connected')
 })
+
+
+
+app.post('/auth/register',(req,res)=>{
+    const {email,password} = req.body
+    const newUser = new User({
+        email,password
+    })
+    newUser.save()
+    return res.json(newUser)
+})
+
+
+
+
+
+
+
+
+
 
 
 app.use(express.json())
